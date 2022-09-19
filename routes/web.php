d406da95346994e55c70452c6f6c0f736b5850b7
Ben M <makingspidersense@gmail.com>
2022-09-19 17:41:50 -0500

    Fixed tabbing in route

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/dashboard', function () {
 require __DIR__.'/auth.php';
 
 // Clear cache without CLI
-    Route::get('/clear-cache', function() {
+Route::get('/clear-cache', function() {
     $exitCode = Artisan::call('config:clear');
     $exitCode = Artisan::call('cache:clear');
     $exitCode = Artisan::call('config:cache');
