33755778107d3ab1970d2c09623d818601e4bcad
Ben M <makingspidersense@gmail.com>
2022-09-19 16:44:24 -0500

    Added /clear-cache route

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+// Clear cache without CLI
+    Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:cache');
+    return 'DONE'; // Return anything
+});
