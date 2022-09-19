ae2883f1829112f7a0858afec3b58aae50d6622d
Ben M <makingspidersense@gmail.com>
2022-09-19 16:43:24 -0500

    Added console_log function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    // Add PHP console logger
+    public function console_log($data) {
+        echo '<script>';
+        echo 'console.log('.json_encode($data).')';
+        echo '</script>';
+    }
+    public function console_log_off($data) {
+        // This is just an easy way to turn hide the console messages
+    }
 }
