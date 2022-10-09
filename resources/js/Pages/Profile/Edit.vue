7964f929100a83eb65e0547141972bb972540748
Ben M <makingspidersense@gmail.com>
2022-10-09 11:02:12 -0500

    Updated profile.edit page with the form

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -9,9 +9,8 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 const form = useForm({
     name: '',
     email: '',
+    nickname: '',
     username: '',
-    password: '',
-    password_confirmation: '',
     terms: false,
 });
 
@@ -27,7 +26,37 @@ const submit = () => {
 
     <AuthenticatedLayout>
         <section class="page-section">
-            Testing Edit Profile
+            <form @submit.prevent="submit">
+                <div>
+                    <InputLabel for="name" value="Name" />
+                    <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
+                    <InputError class="mt-2" :message="form.errors.name" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="email" value="Email" />
+                    <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autocomplete="email" />
+                    <InputError class="mt-2" :message="form.errors.email" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="nickname" value="Nickname" />
+                    <TextInput id="nickname" type="text" class="mt-1 block w-full" v-model="form.nickname" required autocomplete="nickname" />
+                    <InputError class="mt-2" :message="form.errors.nickname" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="username" value="Username" />
+                    <TextInput id="username" type="text" class="mt-1 block w-full" v-model="form.username" required autocomplete="username" />
+                    <InputError class="mt-2" :message="form.errors.username" />
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Update
+                    </PrimaryButton>
+                </div>
+            </form>
         </section>
     </AuthenticatedLayout>
 </template>
