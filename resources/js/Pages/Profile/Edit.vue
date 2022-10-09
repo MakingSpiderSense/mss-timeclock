28c4d96645d75369c919082bf4875bbc84e8448a
Ben M <makingspidersense@gmail.com>
2022-10-09 14:44:29 -0500

    Added profile image input to edit form

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -12,6 +12,7 @@ const form = useForm({
     email: usePage().props.value.auth.user.email,
     nickname: usePage().props.value.auth.user.nickname,
     username: usePage().props.value.auth.user.username,
+    image: null,
 });
 
 const submit = () => {
@@ -24,31 +25,38 @@ const submit = () => {
 
     <AuthenticatedLayout>
         <section class="page-section">
-            <form @submit.prevent="submit">
+            <form @submit.prevent="submit" enctype="multipart/form-data">
+                <!-- Name -->
                 <div>
                     <InputLabel for="name" value="Name" />
-                    <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" autofocus autocomplete="name" />
+                    <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" autofocus required autocomplete="name" />
                     <InputError class="mt-2" :message="form.errors.name" />
                 </div>
-
+                <!-- Email Address -->
                 <div class="mt-4">
                     <InputLabel for="email" value="Email" />
-                    <TextInput id="email" type="text" class="mt-1 block w-full" v-model="form.email" autocomplete="email" />
+                    <TextInput id="email" type="text" class="mt-1 block w-full" v-model="form.email" required autocomplete="email" />
                     <InputError class="mt-2" :message="form.errors.email" />
                 </div>
-
+                <!-- Nickname -->
                 <div class="mt-4">
                     <InputLabel for="nickname" value="Nickname" />
-                    <TextInput id="nickname" type="text" class="mt-1 block w-full" v-model="form.nickname" autocomplete="nickname" />
+                    <TextInput id="nickname" type="text" class="mt-1 block w-full" v-model="form.nickname" required autocomplete="nickname" />
                     <InputError class="mt-2" :message="form.errors.nickname" />
                 </div>
-
+                <!-- Username -->
                 <div class="mt-4">
                     <InputLabel for="username" value="Username" />
-                    <TextInput id="username" type="text" class="mt-1 block w-full" v-model="form.username" autocomplete="username" />
+                    <TextInput id="username" type="text" class="mt-1 block w-full" v-model="form.username" required autocomplete="username" />
                     <InputError class="mt-2" :message="form.errors.username" />
                 </div>
-
+                <!-- Profile Image -->
+                <div class="mt-4">
+                    <InputLabel for="image" value="Profile Image" />
+                    <TextInput id="image" type="file" class="mt-1 block" @input="form.image = $event.target.files[0]" />
+                    <InputError class="mt-2" :message="form.errors.image" />
+                </div>
+                <!-- Submit Button -->
                 <div class="flex items-center justify-end mt-4">
                     <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                         Update
