a5c77b7b503ef49b790b4e14464b49d70b1ea288
Ben M <makingspidersense@gmail.com>
2022-10-17 11:38:54 -0500

    Created components for modal bodies

---
+++ resources/js/Modals/ManualTimeSet.vue
@@ -0,0 +1,18 @@
+<!-- Template -->
+<template>
+    <div id="modalbody-org-create">
+        <div class="mb-4">Enter a manual time to use when clocking in/out. This will be override the actual time.</div>
+        <input id="manual-time-input" type="time">
+    </div>
+</template>
+
+<!-- Scripts -->
+<script>
+export default {
+    name: 'ManualTimeSet',
+}
+</script>
+
+<!-- Styling -->
+<style lang="scss" scoped>
+</style>
---
+++ resources/js/Modals/OrgCreate.vue
@@ -0,0 +1,17 @@
+<!-- Template -->
+<template>
+    <div id="modalbody-org-create">
+        This is body text of the OrgCreate modal.
+    </div>
+</template>
+
+<!-- Scripts -->
+<script>
+export default {
+    name: 'OrgCreate',
+}
+</script>
+
+<!-- Styling -->
+<style lang="scss" scoped>
+</style>
