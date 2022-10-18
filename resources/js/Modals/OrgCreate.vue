4c3ae77b293fd54e66db4763654376c2132940eb
Ben M <makingspidersense@gmail.com>
2022-10-18 18:13:52 -0500

    Updated OrgCreate modal styling/frontend

--- resources/js/Modals/OrgCreate.vue
+++ resources/js/Modals/OrgCreate.vue
@@ -1,7 +1,29 @@
+<script setup>
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { useForm } from '@inertiajs/inertia-vue3';
+
+const form = useForm({
+    orgCreate: '',
+});
+
+const submit = () => {
+    form.post(route('placeholder'), {
+        onFinish: () => form.reset('orgCreate'),
+    });
+};
+</script>
+
 <!-- Template -->
 <template>
     <div id="modalbody-org-create">
-        This is body text of the OrgCreate modal.
+        <div class="mb-4">Add a new organization below. Please note that at this time, organizations can only be edited or deleted directly from the database.</div>
+        <div>
+            <InputLabel for="orgCreate" value="Organization Name" />
+            <TextInput id="org_create" type="text" class="mt-1 block w-full" v-model="form.orgCreate" required />
+            <InputError class="mt-2" :message="form.errors.orgCreate" />
+        </div>
     </div>
 </template>
 
