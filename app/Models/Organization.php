826d264903429afa8fc39230aa32701819837adf
Ben M <makingspidersense@gmail.com>
2022-10-20 23:03:58 -0500

    Created Cateogory model and relationship with Organization

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -19,4 +19,11 @@ class Organization extends Model
         // "An organization can have many users that subscribe to it."
         return $this->belongsToMany(User::class, 'user_org_subscriptions', 'org_id', 'user_id');
     }
+
+    public function organizations()
+    {
+        // Sets up relationship with Category model.
+        // "An organization can have many categories."
+        return $this->hasMany(Category::class)->orderBy('name');
+    }
 }
