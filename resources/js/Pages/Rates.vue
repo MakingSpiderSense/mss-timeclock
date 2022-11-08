71db7c32b95626f181a559ce20c949fabf63c68c
Ben M <makingspidersense@gmail.com>
2022-11-08 17:51:05 -0600

    Styled Rates page

--- resources/js/Pages/Rates.vue
+++ resources/js/Pages/Rates.vue
@@ -8,7 +8,53 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 
     <AuthenticatedLayout>
         <section class="page-section">
-            This is the hourly rates page.
+            <!-- Global and Organization Rates -->
+            <h2>Global and Organization Rates</h2>
+            <div class="parent-rates mb-10">
+                <table class="standard-table table-auto">
+                    <tbody>
+                        <tr>
+                            <td><a href="#">Edit</a></td>
+                            <td>Global rate</td>
+                            <td>$50.00</td>
+                        </tr>
+                        <tr>
+                            <td><a href="#">Edit</a></td>
+                            <td>Client With A Really Very Long Name</td>
+                            <td>$23.50</td>
+                        </tr>
+                        <tr>
+                            <td><a href="#">Edit</a></td>
+                            <td>Making Spider Sense</td>
+                            <td>Not Set</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- Category Rates -->
+            <h2>Category Rates</h2>
+            <p class="mb-4"><em>Only the category that are part of the currently selected organization, <strong>Making Spider Sense</strong>, will show up here.</em></p>
+            <div class="child-rates">
+                <table class="standard-table table-auto">
+                    <tbody>
+                        <tr>
+                            <td><a href="#">Edit</a></td>
+                            <td>Administration</td>
+                            <td>$50.00</td>
+                        </tr>
+                        <tr>
+                            <td><a href="#">Edit</a></td>
+                            <td>Emails</td>
+                            <td>$23.50</td>
+                        </tr>
+                        <tr>
+                            <td><a href="#">Edit</a></td>
+                            <td>Education</td>
+                            <td>Not Set</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </section>
     </AuthenticatedLayout>
 </template>
@@ -24,4 +70,28 @@ export default {
 <!-- Styling -->
 <style lang="scss" scoped>
 @import "../../sass/app.scss";
+table.standard-table {
+    border: none;
+    width: 100%;
+    max-width: 600px;
+    tr {
+        td {
+            padding: 5px;
+            border: none;
+            vertical-align: initial;
+            // Edit Buttons
+            &:first-child {
+                text-align: left;
+            }
+            // Title
+            &:nth-child(2) {
+                text-align: left;
+            }
+            // Rate
+            &:nth-child(3) {
+                text-align: right;
+            }
+        }
+    }
+}
 </style>
