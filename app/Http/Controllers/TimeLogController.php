12029d2ae92ce5432b903eb4aacc1a476b59a195
Ben M <makingspidersense@gmail.com>
2023-04-19 12:53:21 -0500

    Added time log table that pulls in actual log history

--- app/Http/Controllers/TimeLogController.php
+++ app/Http/Controllers/TimeLogController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TimeLogController extends Controller
 {
@@ -15,7 +17,30 @@ class TimeLogController extends Controller
     // Load the time log page
     public function index()
     {
-        // Return the Time Log page
-        return inertia('Reports/TimeLog');
+        // Get user's log data
+        // For each entry in the `temp_logs` table for the user (user_id), we need to get the id, subcategory_id, clock_in, clock_out, minutes, and notes. But then we also need to use the subcategory_id to look into the `subcategories` table to get the category_id and name. Then we need to use the category_id to look into the `categories` and get the org_id and name. Finally, we need to use the org_id to look into the `organizations` table and get the name. We need to do this so that we can display the category and organization names on the time log page. We also need to order the results by clock_in in descending order.
+        $user_id = auth()->user()->id;
+        $logs = DB::table('temp_logs')
+            ->join('subcategories', 'temp_logs.subcategory_id', '=', 'subcategories.id')
+            ->join('categories', 'subcategories.category_id', '=', 'categories.id')
+            ->join('organizations', 'categories.org_id', '=', 'organizations.id')
+            ->leftJoin('users', 'temp_logs.user_id', '=', 'users.id')
+            ->select('temp_logs.id', 'temp_logs.subcategory_id', 'temp_logs.clock_in', 'temp_logs.clock_out', 'temp_logs.minutes', 'temp_logs.notes', 'subcategories.category_id', 'subcategories.name as subcategory_name', 'categories.org_id', 'categories.name as category_name', 'organizations.name as organization_name', 'users.time_zone')
+            ->where('temp_logs.user_id', $user_id)
+            ->orderByDesc('temp_logs.clock_in')
+            ->get();
+
+        // Adjust each log's clock_in and clock_out times to the user's time zone if they have one set
+        foreach ($logs as $log) {
+            if ($log->time_zone) {
+                $log->clock_in = Carbon::parse($log->clock_in)->timezone($log->time_zone);
+                $log->clock_out = Carbon::parse($log->clock_out)->timezone($log->time_zone);
+            }
+        }
+
+        // Return the Time Log page with the variables
+        return inertia('Reports/TimeLog', [
+            'logs' => $logs,
+        ]);
     }
 }
--- resources/js/Pages/Reports/TimeLog.vue
+++ resources/js/Pages/Reports/TimeLog.vue
@@ -22,17 +22,118 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 
     <AuthenticatedLayout :pageModal="pageModal">
         <section class="page-section">
-            <h2>Time Log</h2>
-            History of time logs go here
+            <h2 style="margin-bottom: 25px;">Time Log</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Organization</th>
+                        <th>Category</th>
+                        <th>Subcategory</th>
+                        <th>Time In</th>
+                        <th>Time Out</th>
+                        <th>Total Time</th>
+                        <th>Notes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="log in logs" :key="log.id">
+                        <td :title="`Id: ${log.id}, Org: ${log.org_id}, Category: ${log.category_id}, Subcategory: ${log.subcategory_id}`">{{ formatDate(log.clock_in) }}</td>
+                        <td>{{ log.organization_name }}</td>
+                        <td>{{ log.category_name }}</td>
+                        <td>{{ log.subcategory_name }}</td>
+                        <td>{{ formatTime(log.clock_in) }}</td>
+                        <td>{{ formatTime(log.clock_out) }}</td>
+                        <td>{{ formatMinutes(log.minutes) }}</td>
+                        <td :title="log.notes">{{ formatNotes(log.notes) }}</td>
+                    </tr>
+                </tbody>
+            </table>
         </section>
     </AuthenticatedLayout>
 </template>
 
 <!-- Scripts -->
 <script>
+    export default {
+        props: ['logs'],
+        methods: {
+            // Format date to mm/dd/yyyy
+            formatDate(dateString) {
+                const date = new Date(dateString);
+                return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+            },
+            // Format time to 12 hour format with am/pm
+            formatTime(dateString) {
+                if (dateString) {
+                    const date = new Date(dateString);
+                    const hours = date.getHours();
+                    const minutes = date.getMinutes().toString().padStart(2, '0');
+                    const amOrPm = hours < 12 ? 'am' : 'pm';
+                    const twelveHourFormat = hours % 12 || 12;
+                    return `${twelveHourFormat}:${minutes}${amOrPm}`;
+                }
+                return '';
+            },
+            // Format minutes to hours and minutes
+            formatMinutes(minutes) {
+                if (minutes) {
+                    const hours = Math.floor(minutes / 60);
+                    const remainingMinutes = minutes % 60;
+                    return `${hours}h ${remainingMinutes}m`;
+                } else if (minutes === 0) {
+                    return '0h 0m';
+                } else {
+                    return 'N/A';
+                }
+            },
+            // Format notes to only show the first 10 characters and then a "..." after
+            formatNotes(notes) {
+                if (notes) {
+                    return notes.length > 10 ? `${notes.substring(0, 10)}...` : notes;
+                }
+                return '';
+            },
+        },
+    }
 </script>
 
 <!-- Styling -->
 <style lang="scss" scoped>
 @import "../../../sass/app.scss";
+// Table
+// Eventually move this to a global style sheet if it's used in multiple places
+table, td, th {  
+    border: 1px solid #ddd;
+    text-align: center;
+}
+table {
+    border-collapse: collapse;
+    width: 100%;
+    margin-bottom: 50px;
+    // th {
+    //     cursor: pointer;
+    // }
+    th, td {
+        padding: 8px 15px;
+    }
+    .notes {
+        text-align: left;
+    }
+    .price {
+        text-align: right;
+    }
+}
+@media (max-width: 1000px) {
+    table {
+        display: block;
+        overflow-x: auto;
+        white-space: nowrap;
+        th, td {
+            img {
+                max-width: 50px;
+            }
+        }
+    }
+}
 </style>
