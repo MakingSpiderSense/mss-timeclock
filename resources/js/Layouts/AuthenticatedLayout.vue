1e5d4fbfecb56b6229072081cdd1c79db1875848
Ben M <makingspidersense@gmail.com>
2023-04-19 10:15:46 -0500

    Added Reports navigation items

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -91,9 +91,30 @@ const showingNavigationDropdown = ref(false);
                         </Dropdown>
                     </li>
                     <li>
-                        <NavLink :href="route('placeholder')" :active="route().current('placeholder')">
-                            Reports
-                        </NavLink>
+                        <!-- Dropdown -->
+                        <Dropdown align="left" width="48">
+                            <template #trigger>
+                                <button>
+                                    Reports 
+                                    <!-- Down chevron -->
+                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </button>
+                            </template>
+                            <!-- Dropdown Links -->
+                            <template #content>
+                                <DropdownLink  :href="route('placeholder')" as="button">
+                                    Month Overview
+                                </DropdownLink>
+                                <DropdownLink  :href="route('placeholder')" as="button">
+                                    All Time Overview
+                                </DropdownLink>
+                                <DropdownLink  :href="route('placeholder')" as="button">
+                                    Time Log
+                                </DropdownLink>
+                            </template>
+                        </Dropdown>
                     </li>
                 </ul>
             </nav>
