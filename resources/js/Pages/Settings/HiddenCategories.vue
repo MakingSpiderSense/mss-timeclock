d61a2b976e856bcca8e83acf0da08139f72d36eb
Ben M <makingspidersense@gmail.com>
2024-06-19 13:35:21 -0500

    Update HiddenCategories page layout and styling with fake data

--- resources/js/Pages/Settings/HiddenCategories.vue
+++ resources/js/Pages/Settings/HiddenCategories.vue
@@ -13,8 +13,37 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 
     <AuthenticatedLayout :pageModal="pageModal">
         <section class="page-section">
-            <h2>Hidden Categories</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, blanditiis atque distinctio repellat aspernatur nam cum. Officiis quia dolorum corrupti rerum quis, sed blanditiis, labore, deleniti at totam consequuntur eius ipsa tempora ut quae voluptatum soluta? Excepturi officiis quibusdam corporis, magni laudantium minima doloribus sed ex quis. Veniam, sapiente aspernatur.
+            <h2 style="margin-bottom: 25px;">Hidden Categories</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Organization</th>
+                        <th>Category</th>
+                        <th>Subcategory</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Acme Co</td>
+                        <td>Business Administration</td>
+                        <td>* All subcategories hidden</td>
+                        <td><Link class="btn">Unhide</Link></td>
+                    </tr>
+                    <tr>
+                        <td>Acme Co</td>
+                        <td>Business Improvements</td>
+                        <td>Marketing</td>
+                        <td><Link class="btn">Unhide</Link></td>
+                    </tr>
+                    <tr>
+                        <td>Personal</td>
+                        <td>Side Projects</td>
+                        <td>* All subcategories hidden</td>
+                        <td><Link class="btn">Unhide</Link></td>
+                    </tr>
+                </tbody>
+            </table>
         </section>
     </AuthenticatedLayout>
 </template>
@@ -26,4 +55,36 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 <!-- Styling -->
 <style lang="scss" scoped>
 @import "../../../sass/app.scss";
+// Table
+// Eventually move this to a global style sheet if it's used in multiple places
+table, td, th {
+    border: 1px solid #ddd;
+    text-align: center;
+}
+table {
+    border-collapse: collapse;
+    width: 100%;
+    margin-bottom: 50px;
+    th, td {
+        padding: 8px 15px;
+    }
+    .notes {
+        text-align: left;
+    }
+    .price {
+        text-align: right;
+    }
+}
+@media (max-width: 1000px) {
+    table {
+        display: block;
+        overflow-x: auto;
+        white-space: nowrap;
+        th, td {
+            img {
+                max-width: 50px;
+            }
+        }
+    }
+}
 </style>
