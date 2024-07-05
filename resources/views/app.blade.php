36c40c3520b22fa46b8a353965cc057a14f6a380
Ben M <makingspidersense@gmail.com>
2024-07-05 15:22:37 -0500

    Add CSRF token to app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,9 @@
         <!-- Favicon -->
         <link rel="shortcut icon" href="{{ url('img/logo-timeclock.png') }}">
 
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <!-- Scripts -->
         @routes
         @vite('resources/js/app.js')
