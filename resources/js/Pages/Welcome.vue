2a676c1cab12b047882ce31f110a710e8e33285b
Ben M <makingspidersense@gmail.com>
2024-11-25 19:02:37 -0500

    Improve styling of login page on mobile

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,8 +13,8 @@ defineProps({
 <template>
     <Head title="Welcome" />
 
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+    <div class="login-container items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div v-if="canLogin" class="login-nav px-6 py-4">
             <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</Link>
 
             <template v-else>
@@ -24,7 +24,7 @@ defineProps({
             </template>
         </div>
 
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div class="login-logo max-w-6xl mx-auto sm:px-6 lg:px-8">
             <!-- Logo -->
             <div class="flex justify-center pt-8 sm:pt-0">
                 <div class="logo-text">
@@ -49,4 +49,21 @@ defineProps({
 <!-- Styling -->
 <style lang="scss" scoped>
 @import "../../sass/app.scss";
+.login-container {
+    display: flex;
+    flex-direction: column;
+}
+.login-nav {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+}
+.login-logo {
+    display: flex;
+    flex: 1;
+    transform: translateY(-35px);
+    .logo-text {
+        align-self: center;
+    }
+}
 </style>
